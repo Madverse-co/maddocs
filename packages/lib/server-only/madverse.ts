ef86965ef551156a912310a00753521c1bdd3a1c
siddnikh <mailtosiddnikh@gmail.com>
2025-03-03 22:20:25 +0530

    fix: puppeteer stuff

--- packages/lib/server-only/madverse.ts
+++ packages/lib/server-only/madverse.ts
@@ -1,6 +1,8 @@
 import chromium from '@sparticuz/chromium-min';
 import { PDFDocument } from 'pdf-lib';
-import puppeteer from 'puppeteer-core';
+import puppeteer from 'puppeteer';
+import { type Browser } from 'puppeteer';
+import { type Browser as BrowserCore } from 'puppeteer-core';
 
 import { labelInvite } from './madverse-templates';
 
@@ -140,15 +142,23 @@ export async function generatePdf({ labelName, labelAddress }: GeneratePdfParams
       labelAddress,
     );
 
-    // Use puppeteer in memory without writing to disk
-    const browser = await puppeteer.launch({
-      args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(
-        `https://github.com/Sparticuz/chromium/releases/download/v132.0.0/chromium-v132.0.0-pack.tar`,
-      ),
-      headless: 'shell',
-    });
+    let browser: Browser | BrowserCore;
+    if (process.env.NODE_ENV === 'production' || process.env.VERCEL_ENV === 'production') {
+      browser = await puppeteer.launch({
+        args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath(
+          `https://github.com/Sparticuz/chromium/releases/download/v132.0.0/chromium-v132.0.0-pack.tar`,
+        ),
+        headless: 'shell',
+      });
+    } else {
+      browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
+    }
+
     console.log('generatePdf: Browser launched');
     const page = await browser.newPage();
 
