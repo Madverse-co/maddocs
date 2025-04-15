2823202b824599c6f2ece9992edff0a8a61e82ff
siddnikh <siddharth.nikhil@madverse.co>
2025-04-15 08:44:53 +0530

    chore: revamp emails

--- packages/email/template-components/template-footer.tsx
+++ packages/email/template-components/template-footer.tsx
@@ -1,24 +1,58 @@
-import { Trans } from '@lingui/macro';
-
-import { Section, Text } from '../components';
-import { useBranding } from '../providers/branding';
+import { Column, Img, Link, Row, Section, Text } from '../components';
 
 export type TemplateFooterProps = {
   isDocument?: boolean;
+  assetBaseUrl?: string;
 };
 
-export const TemplateFooter = ({ isDocument = true }: TemplateFooterProps) => {
-  const branding = useBranding();
-
+export const TemplateFooter = ({
+  isDocument = true,
+  assetBaseUrl = 'http://localhost:3002',
+}: TemplateFooterProps) => {
   return (
-    <Section>
-      {isDocument && !branding.brandingHidePoweredBy && (
-        <Text className="my-4 text-base text-slate-400">
-          <Trans>
-            This document was sent using <Text className="text-[#7AC455]">Maddocs.</Text>
-          </Trans>
-        </Text>
-      )}
+    <Section className="mt-8 bg-black px-6 py-8 text-white">
+      <Row>
+        <Column className="w-1/3 align-middle">
+          <Img
+            src="https://app.madverse.com/static/logo-onboarding.png"
+            alt="MADVERSE"
+            width="160"
+            className="mb-0 mt-0"
+          />
+        </Column>
+
+        <Column className="w-2/3 align-middle">
+          <Text className="m-0 text-base font-light text-white">
+            Tower A, Golf View Corporate Towers,
+          </Text>
+          <Text className="m-0 text-base font-light text-white">
+            Golf Course Road, Gurgaon, Haryana, India, 122002
+          </Text>
+        </Column>
+      </Row>
+
+      <Row className="mt-4">
+        <Column>
+          <Row>
+            <Column className="w-auto pr-6">
+              <Link
+                href="https://www.instagram.com/madverse.music/"
+                className="text-white no-underline"
+              >
+                <Text className="m-0 text-sm font-light text-white">Instagram</Text>
+              </Link>
+            </Column>
+            <Column className="w-auto">
+              <Link
+                href="https://in.linkedin.com/company/madverse-music"
+                className="text-white no-underline"
+              >
+                <Text className="m-0 text-sm font-light text-white">Linkedin</Text>
+              </Link>
+            </Column>
+          </Row>
+        </Column>
+      </Row>
     </Section>
   );
 };
